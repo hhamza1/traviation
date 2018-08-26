7a0645bc3a2ae11e89a24d58783ca607dda44f34
hhamza1 <hamzahmitou@gmail.com>
2018-08-26 03:02:05 +0300

    Create modular tasks

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    browserSynch = require('browser-sync').create();
+
+gulp.task('html', function(done){
+    browserSynch.reload();
+    done();
+});
+
+gulp.task('watch', function(){
+
+    browserSynch.init({
+        notify: false,
+        server:{
+            baseDir: "app"
+        }
+    });
+
+    gulp.watch('./app/index.html', gulp.series('html'));
+    gulp.watch('./app/assets/styles/**/*.css', gulp.series('cssInject'));
+});
+
+
+gulp.task('cssInject', gulp.series('styles', function(){
+    return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSynch.stream());
+})); 
