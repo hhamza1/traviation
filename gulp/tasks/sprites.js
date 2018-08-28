7fe0d9c4805cb45469089894c5d400f70f9e666f
hhamza1 <hamzahmitou@gmail.com>
2018-08-29 02:46:49 +0300

    Add & Config Sprite module

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,2 +1,23 @@
 var gulp =  require('gulp'),
     sprite = require('gulp-svg-sprite'); 
+
+var config = {
+    mode: {
+        css:{
+            render: {
+                css:{
+                    template:'./gulp/templates/sprite.css'
+                }
+            }
+        }
+    }
+}
+
+
+
+gulp.task('createSprite', function(done){
+    gulp.src('./app/assets/images/icons/**/*.svg')
+    .pipe(sprite(config))    
+    .pipe(gulp.dest('./app/temp'));
+    done();
+});
