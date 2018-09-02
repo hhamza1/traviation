de405c13f4afbd34ebffe49ac915e092341df5cc
hhamza1 <hamzahmitou@gmail.com>
2018-09-02 20:28:43 +0300

    Mobile menu jQuery

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,6 +1,21 @@
+import $ from 'jquery';
 class MobileMenu {
     constructor(){
-        alert('New mobile menu');
+        this.siteHeader = $('.site-header');
+        this.menuIcon = $('.site-header__menu-icon');
+        this.menuContent = $('.site-header__menu-content');
+        this.events();
+        
+    }
+
+    events(){
+        this.menuIcon.click(this.toggleMenu.bind(this));
+    }
+
+    toggleMenu() {
+        this.menuContent.toggleClass('site-header__menu-content--is-visible');
+        this.siteHeader.toggleClass('site-header--is-expanded');
+        this.menuIcon.toggleClass('site-header__menu-icon--close-icon');
     }
 
 }
